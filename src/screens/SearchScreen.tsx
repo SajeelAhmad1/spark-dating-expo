f611a6ff63a6ff7161c6847a56275f66482f7492
wajidalise <officewajidali@gmail.com>
2026-06-30 02:01:13 +0500

    flow changed after login ++

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -26,12 +26,9 @@ const SearchScreen = ({ navigation }: any) => {
   const { data, isPending, isError, refetch, isFetching } = useDiscoverProfiles(
     coords ? { lat: coords.lat, lng: coords.lng, limit: 10 } : null,
   );
-  console.log(data?.profiles?.length, 'profiles length discoveryscreen');
-  console.log(data, 'data Searchscreen');
-  console.log(isError, 'isError Searchscreen');
-  useEffect(() => {
-    console.log("coords", coords);
-console.log("isLoading", isLoading);
+  // console.log(data?.profiles?.length, 'profiles length discoveryscreen');
+  
+  useEffect(() => { 
     if (!coords && !isLoading) {
       navigation.replace('EnableLocationScreen');
       return;
